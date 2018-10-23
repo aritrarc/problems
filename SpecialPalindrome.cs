b7f409799ce63487aeceeb382f8a5c06c922a5f4
Aritra Roychoudhury <roycha@ucles.internal>
2018-10-23 15:00:38 +0530

    Added comments to Special palindrome solution

--- SpecialPalindrome.cs
+++ SpecialPalindrome.cs
@@ -22,28 +22,50 @@ namespace SpecialPalindrome
         private static int palindromeRecurse(string str){
 
             int n = str.Length;
+             // store count of special Palindromic substring 
             int result = 0;
+            // it will store the count of continues same char 
             int[] sameChar = new int[n];
             int i = 0;
+
+             // traverse string character from left to right 
             while (i < n)
             {
+                // store same character count 
                 int samecharCount = 1;
                 int j = i + 1;
+
+                 // count smiler character 
                 while( j < n && str[i] == str[j] )
                 {
                     j++; samecharCount++;
                 }
+
+                // Case : 1 
+                // so total number of substring that we can 
+                // generate are : K *( K + 1 ) / 2 
+                // here K is sameCharCount 
                 result += (samecharCount * (samecharCount + 1)/2);
+
+                // store current same char count in sameChar[] 
+                // array 
                 sameChar[i] = samecharCount;
                 i = j;
             } 
 
+               // Case 2: Count all odd length Special Palindromic 
+                // substring
             for( int  j=1; j<n; j++)
             {
+
+                // if current character is equal to previous 
+                // one then we assign Previous same character 
+                // count to current one 
                 if(str[j] == str[j - 1]){
                     sameChar[j] = sameChar[j - 1];
                 }
 
+                // case 2: odd length
                 if(j > 0 && j < (n - 1) && 
                 (str[j - 1] == str[j + 1] && str[j] != str[j - 1])) {
                     result += Math.Min(sameChar[j - 1], 
