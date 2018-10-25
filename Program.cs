94f9515d3f62b04a95e2ea17cc8613a63f78d5ba
aritrarc <ari.royc2008@gmail.com>
2018-10-25 20:15:51 +0530

    Added Sorting file

--- Program.cs
+++ Program.cs
@@ -11,8 +11,9 @@ namespace HelloWorld
             // RemoveCharacters.RemoveCharacters.RemoveCharacterFromString("geeksforgeek", 'g');
             //ReverseString.ReverseString.RemoveCharacterFromString("abcdefg");
             //SpecialPalindrome.SpecialPalindrome.FindSpecialPalindromeCount("nnonnpoo");
-            SpecialPalindrome.SpecialPalindrome.FindSpecialPalindromeCount("aaaa");
+            //SpecialPalindrome.SpecialPalindrome.FindSpecialPalindromeCount("aaaa");
             //Solution.whatFlavors(new int[]{2, 2, }, 4);
+            Sorting.BubbleSort(new int[]{5, 8, 12, 10, 4, 6});
         }
     }
 }
---
+++ Sorting.cs
@@ -0,0 +1,61 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+public class Sorting {
+
+    // Various sorting algorithms
+    public static void SelectionSort(int[] arr) {
+        Console.WriteLine("Selection Sort");
+
+        for(int i=0; i<arr.Length; i++)
+        {
+            for(int j=i+1; j <arr.Length; j++)
+            {
+                if(arr[j] < arr[i])
+                {
+                    int temp = arr[j];
+                    arr[j] = arr[i];
+                    arr[i] = temp;
+                }
+            }
+        }
+        PrintSortedArray(arr);
+    }
+
+    public static void BubbleSort(int[] arr) {
+        Console.WriteLine("Bubbble Sort");
+
+        for(int i=arr.Length -1; i >= 0 ; i--)
+        {
+            for(int j=i-1; j >= 0; j--)
+            {
+                if(arr[j] >  arr[i])
+                {
+                    int temp = arr[j];
+                    arr[j] = arr[i];
+                    arr[i] = temp;
+                }
+            }
+        }
+        PrintSortedArray(arr);
+    }
+
+    private static void PrintSortedArray(int[] arr){
+
+        for(int x = 0; x < arr.Length; x++)
+        {
+            Console.WriteLine(arr[x]);
+        }
+    }
+}
