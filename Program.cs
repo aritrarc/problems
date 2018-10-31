a474be417178de81f5500bd906203215b0154902
Aritra Roychoudhury <roycha@ucles.internal>
2018-10-31 09:50:25 +0530

    Temp commit

--- Program.cs
+++ Program.cs
@@ -14,7 +14,8 @@ namespace HelloWorld
             //SpecialPalindrome.SpecialPalindrome.FindSpecialPalindromeCount("aaaa");
             //Solution.whatFlavors(new int[]{2, 2, }, 4);
             //Sorting.BubbleSort(new int[]{5, 8, 12, 10, 4, 6});
-            Solution.BinarySearch(new int[]{7, 2, 5, 4, 11}, 12);
+            //Solution.BinarySearch(new int[]{7, 2, 5, 4, 11}, 12);
+            MinimumTImeRequired.LCM(new long[]{1, 3, 4}, 10);
         }
     }
 }
