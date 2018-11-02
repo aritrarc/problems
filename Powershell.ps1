3c2a01fd614628b074672e97be7828bbced2dcb6
Aritra Roychoudhury <roycha@ucles.internal>
2018-11-02 12:02:26 +0530

    New commands for Out-Gridview

--- Powershell.ps1
+++ Powershell.ps1
@@ -6,3 +6,9 @@ $centralUSResources = {}
 #Get-AzureRmResourceGroup 'RsgDEVCEMetrica'
 
 $centralUSResources = Get-AzureRmResource | Where-Object Location -eq 'CentralUS'
+
+$Menu = 'RsgDEVCEMetrica', 'RsgSelCEMetrica'
+
+$ApplicationChoices = $Menu | Out-GridView -OutputMode Single -Title 'Select application to deploy , and click OK.'
+
+# $ApplicationChoices = Out-GridView -InputObject $Menu -OutputMode Single -Title 'Select application to deploy , and click OK.'
