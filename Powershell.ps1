4415a770ee1636368946e14dc5f031c829f22c67
Aritra Roychoudhury <roycha@ucles.internal>
2018-11-02 19:26:42 +0530

    Verbose

--- Powershell.ps1
+++ Powershell.ps1
@@ -1,5 +1,8 @@
 #Powershell
 
+#PS version
+$PSVersionTable.PSVersion
+
 #Define variables
 $centralUSResources = {}
 
@@ -7,8 +10,29 @@ $centralUSResources = {}
 
 $centralUSResources = Get-AzureRmResource | Where-Object Location -eq 'CentralUS'
 
+#Out-GridView
+
 $Menu = 'RsgDEVCEMetrica', 'RsgSelCEMetrica'
 
 $ApplicationChoices = $Menu | Out-GridView -OutputMode Single -Title 'Select application to deploy , and click OK.'
 
-# $ApplicationChoices = Out-GridView -InputObject $Menu -OutputMode Single -Title 'Select application to deploy , and click OK.'
+Switch ($ApplicationChoices)
+{
+    RsgDEVCEMetrica {
+        Get-AzureRmResourceGroup -Name $ApplicationChoices
+    }
+
+    RsgSelCEMetrica {
+        Get-AzureRmResourceGroup -Name $ApplicationChoices
+    }
+}
+
+#Credential
+$cred = Get-Credential -Message 'Enter your credentials'
+
+#Write to console
+$VerbosePreference.value__
+Write-Verbose -Message "Searching the Application Event Log."
+Write-Host $cred
+
+
