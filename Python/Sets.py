aaeb4fdbc8d55087b93d4a0fea8d4d2b51f828a2
Aritra Roychoudhury <roycha@ucles.internal>
2019-02-18 18:16:00 +0530

    Added python set operation problems

--- Python/Sets.py
+++ Python/Sets.py
@@ -22,3 +22,50 @@ D =  B.difference(C)
 E =  C.difference(B)
 print(sum([ 1 if i in D else -1 if i in E else 0 for i in A]))
 print(sum([(i in B) - (i in C) for i in A]))
+
+# Set.add()
+set_1 = set([])
+# n = int(input())
+# for _ in range(n):
+#     set_1.add(input())
+print(len(set_1))
+
+# Set discard , remove , pop
+#-----------------------------
+# n = map(int, input())
+# set_2 = set(map(int, input().split()))
+# c = int(input())
+# for _ in range(c):
+#     cmd = input().split()
+#     operator = cmd[0]
+#     args = cmd[1:]
+#     eval("set_2." +  operator + "(" + ",".join(args) + ")")
+# print(sum(set_2))
+
+#Union
+# ------------------------
+# n = map(int, input())
+# set_1 = set(map(int, input().split()))
+# m = map(int, input())
+# set_2 = set(map(int, input().split()))
+# print(len(set_1.union(set_2)))
+
+#Intersection
+# ------------------------
+# n = map(int, input())
+# set_1 = set(map(int, input().split()))
+# m = map(int, input())
+# set_2 = set(map(int, input().split()))
+# print(len(set_1.intersection(set_2)))
+
+#Set Mutations (union_update/ intersection_update/ difference_update/ symmetric_difference_update)
+n = map(int, input())
+set_1 = set(map(int, input().split()))
+q = int(input())
+for _ in range(q):
+    s = input()
+    set_2 = tuple(map(int, input().split()))
+    eval("set_1." + s + str(set_2))
+    #print(set_2)
+print(set_1)
+
--- Python/print.py
+++ Python/print.py
@@ -1,3 +1,9 @@
 # Print each element of a list on a separate Line using List comprehension
 A = [1, 2, 3, 4]
 [print(i, end="\n") for i in A]
+
+s1 = set(map(int, input().split()))
+s2 = "intersection_update"
+s3 = (2, 3, 4)
+print(s1)
+eval("s1." + s2 + "s3" )
